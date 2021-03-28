8ad9a8a3f121731be483e7b1d601d581c66fdcc4
50start <seriebig18@outlook.jp>
2021-03-29 05:58:45 +0900

    jisenn 10 finish

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDgpVqJ1HFDJtBXugD4ww1r3fvuXQG5-z4",
+  authDomain: "chatbot-demoshin.firebaseapp.com",
+  projectId: "chatbot-demoshin",
+  storageBucket: "chatbot-demoshin.appspot.com",
+  messagingSenderId: "781358747304",
+  appId: "1:781358747304:web:75c1616d93b9896b4f3c9d",
+  measurementId: "G-3E97LN3HLY"
+};
+
+export default firebaseConfig
